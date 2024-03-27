0605144dd416a825a43055da36ea1b5a8ae60bea
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2024-03-27 22:35:57 +0800

    Implemented initial Register, Login, and Delete functionalities for accounts

---
+++ src/controllers/accounts.js
@@ -0,0 +1,73 @@
+var bcrypt = require('bcryptjs');
+const Account = require('../models/Account')
+const express = require('express');
+const router = express.Router();
+
+// Register an account
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).send('Please provide username and password.');
+    }
+
+    try {
+        const existingAccount = await Account.findOne({ username });
+
+        if (existingAccount) {
+        return res.status(409).send('Account already exists in the database.');
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        
+        const newAccount = new Account({ username, hash});
+
+        await newAccount.save();
+        res.status(201).send({ message: "Account created successfully" });;
+    } catch (error) {
+        res.status(400).send({ error: "Registration failed", details: error.message }); 
+    }
+});
+
+// Login an account
+router.get('/login', async (req, res) => {
+    try {
+        const user = await Account.findOne({username: req.body.username})
+        if (!user) return next(createError(400, "Incorrect username or password"))
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return next(createError(400, "Incorrect username or password"))
+
+    } catch (error) {
+        res.status(400).send({ error: "Login failed", details: error.message});
+    }
+})
+
+// Get all properties
+router.get('/getallaccounts', async (req, res) => {
+    try {
+        const accounts = await Account.find().lean().exec();
+
+        const accountDetails = accounts.map(account => ({
+            username: account.username,
+            password: account.password
+        }));
+
+        res.render('accounts', { accounts: accountDetails });
+    } catch (e) {
+        res.status(500).send({error: "Fetch Accounts failed", details: e.message});
+    }
+});
+
+// Delete an account
+router.post('/deleteaccount', async (req, res) => {
+    const status = await Account.deleteOne({name: req.body.username})
+    if (!status) {
+        res.status(400).send("Error occurred during deleting");
+    } else {
+        res.status(200).send("Account Deleted");
+    }
+})
+
+module.exports = router;
