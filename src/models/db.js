d9bc0ac7d8703f5dfed945730f32093545b654aa
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2024-04-05 12:31:57 +0800

    Reverted db model

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 function connect() {
-    return mongoose.connect(process.env.MONGODB_URI, { 
-        maxPoolSize: 10 // This is the max number of connections in the pool
-    });
+    return mongoose.connect(process.env.MONGODB_URI);
 }
 
 module.exports = connect;
